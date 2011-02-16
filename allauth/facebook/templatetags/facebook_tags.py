d4539ee7ac8e59d34d02af0d2a06b61305c35182
Chris Beaven <smileychris@gmail.com>
2011-02-17 11:58:17 +1300

    Make the lack of a FacebookApp corresponding to the current site fail silently rather than raising an exception.

--- allauth/facebook/templatetags/facebook_tags.py
+++ allauth/facebook/templatetags/facebook_tags.py
@@ -1,10 +1,10 @@
 from django import template
-from django.conf import settings
 
 from allauth.facebook.models import FacebookApp
 from allauth.socialaccount.app_settings import QUERY_EMAIL
 
 register = template.Library()
+
 
 @register.inclusion_tag('facebook/fbconnect.html', takes_context=True)
 def fbconnect(context):
@@ -14,4 +14,3 @@ def fbconnect(context):
     perms = ','.join(perm_list)
     return {'facebook_app': FacebookApp.objects.get_current(),
             'facebook_perms': perms}
-
