5ed0a96099b57b4d7d7c31fd6c085d1a1cfe2814
Chris Beaven <smileychris@gmail.com>
2011-02-17 11:58:17 +1300

    Make the lack of a FacebookApp corresponding to the current site fail silently rather than raising an exception.

--- allauth/facebook/models.py
+++ allauth/facebook/models.py
@@ -1,14 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 
 from allauth.socialaccount.defs import SocialAccountProvider
 from allauth.socialaccount.models import SocialAccount
 
+
 class FacebookAppManager(models.Manager):
+
     def get_current(self):
+        """
+        Get the FacebookApp corresponding to the current site.
+
+        :returns: A :class:`FacebookApp` instance or ``None`` if no match is
+            found.
+        """
         site = Site.objects.get_current()
-        return self.get(site=site)
+        try:
+            return self.get(site=site)
+        except FacebookApp.DoesNotExist:
+            pass
+
 
 class FacebookApp(models.Model):
     objects = FacebookAppManager()
@@ -21,6 +32,7 @@ class FacebookApp(models.Model):
 
     def __unicode__(self):
         return u"%s (@%s)" % (self.name, self.site)
+
 
 class FacebookAccount(SocialAccount):
     social_id = models.CharField(max_length=255, blank=False, unique=True)
@@ -35,4 +47,3 @@ class FacebookAccount(SocialAccount):
 
     def __unicode__(self):
         return "%s (%s)" % (self.name, self.social_id)
-    
