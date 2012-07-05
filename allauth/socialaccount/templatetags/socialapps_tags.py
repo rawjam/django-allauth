7f00f594a6e8639f9155482a7a3b2868777568f6
Nelson <work@nelsonmonteiro.net>
2012-07-05 12:38:59 +0100

    adding socialapps_tags.py

---
+++ allauth/socialaccount/templatetags/socialapps_tags.py
@@ -0,0 +1,10 @@
+from django.template.defaulttags import token_kwargs
+from django import template
+
+from allauth.socialaccount.models import SocialApp
+
+register = template.Library()
+
+@register.assignment_tag
+def get_provider_app(provider):
+    return SocialApp.objects.get_current(provider)
